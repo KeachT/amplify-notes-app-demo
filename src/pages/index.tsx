ad82cc50a1eacc73175619aa9cd5793c58dd0888
KeachT <503.b03@gmail.com>
2023-11-30 00:50:30 +0900

    add env

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,9 +8,6 @@ import { Note } from '../features/note'
 export default function Home() {
   return (
     <Layout title="Home">
-      {/* TODO 最後に消す、CICD動作確認用 */}
-      <div>Home</div>
-
       <Stack align="center">
         <Title>Notes</Title>
         <Note />
